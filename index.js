bab7abf6c19891f5cf2156d1550ebec83117a3a4
DivyaanSH23 <divyanshshuklaofficial23@gmail.com>
2024-05-10 19:28:43 +0530

    modified UI

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const path = require('path');
 
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/video-call-app');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'landing.html'));
+});
 
 app.listen(3000, ()=>{
     console.log('Server is running');
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,37 +1,38 @@
 const express = require('express');
 const router = express();
-
 const session = require('express-session');
 const { SESSION_SECRET } = process.env;
-router.use(session({ secret:SESSION_SECRET }));
+
+router.use(session({ secret: SESSION_SECRET }));
 
 const userController = require('../controllers/userController');
-
 const auth = require('../middleware/auth');
+const path = require('path');
+const multer = require('multer');
 
-const path=require('path');
-const multer=require('multer');
-
-const storage=multer.diskStorage({
-    destination: function(req,file,cb){
-        cb(null,path.join(__dirname,'../public/images'));
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, path.join(__dirname, '../public/images'));
     },
-    filename:function(req,file,cb){
-        const name=Date.now()+'-'+file.originalname;
-        cb(null,name);
+    filename: function(req, file, cb) {
+        const name = Date.now() + '-' + file.originalname;
+        cb(null, name);
     }
 });
 
-const upload =multer({storage:storage});
+const upload = multer({ storage: storage });
 
-router.get('/', auth.isLogout,userController.loadLogin);
-router.post('/', auth.isLogout,userController.login);
-router.get('/register', auth.isLogout,userController.loadRegister);
-router.post('/register',upload.single('image'), auth.isLogout,userController.register);
-router.get('/home', auth.isLogin,userController.loadHome);
-router.get('/logout', auth.isLogin,userController.logout);
+// Route for the landing page
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'landing.html'));
+});
 
-router.get('/get-user-profile', auth.isLogin,userController.getUserProfile);
-
+router.get('/login', auth.isLogout, userController.loadLogin); // Updated route
+router.post('/login', auth.isLogout, userController.login); // Updated route
+router.get('/register', auth.isLogout, userController.loadRegister);
+router.post('/register', upload.single('image'), auth.isLogout, userController.register);
+router.get('/home', auth.isLogin, userController.loadHome);
+router.get('/logout', auth.isLogin, userController.logout);
+router.get('/get-user-profile', auth.isLogin, userController.getUserProfile);
 
 module.exports = router;
